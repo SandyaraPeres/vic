8c7df480891cb7632bbbf6fd3c6e36fb2bafc061
Sandyara Peres <sandyara.peres@gmail.com>
2021-05-31 21:02:50 -0300

    Aula 09 - Enum classes, unwrap properties e Radio Button

---
+++ Aula 09/app/src/main/java/com/sandyara/aula07/Setor.kt
@@ -0,0 +1,7 @@
+package com.sandyara.aula07
+
+enum class Setor(val id: Int, val descricao: String) {
+    TECNOLOGIA(0, "Tecnologia"),
+    RECURSOS_HUMANOS(1, "Recursos Humanos"),
+    COMERCIAL(2, "Comercial")
+}
